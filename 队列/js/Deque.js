dc8df8f59367433ab5bc546687da2ce89642b558
js-he <cuijiuhe@163.com>
2022-04-10 16:26:54 +0800

    feat: 队列

---
+++ 队列/js/Deque.js
@@ -0,0 +1,25 @@
+class Deque {
+  constructor() {
+    this.count = 0
+    this.items = {}
+    this.lowestCount = 0
+  }
+  addFront(element) { //该方法在双端队列前端添加新的元素
+    
+  }
+  addBack(element) {  //该方法在双端队列后端添加新的元素(实现方法和 Queue 类中的 enqueue 方法相同
+
+  }
+  removeFront() { //该方法会从双端队列前端移除第一个元素(实现方法和 Queue 类中的 dequeue 方法相同)
+
+  }
+  removeBack() {  //该方法会从双端队列后端移除第一个元素(实现方法和 Stack 类中的 pop 方法一样)
+
+  }
+  peekFront() { //该方法返回双端队列前端的第一个元素(实现方法和 Queue 类中的 peek 方法一样)
+
+  }
+  peekBack() {  //该方法返回双端队列后端的第一个元素(实现方法和 Stack 类中的 peek 方法一样)
+
+  }
+}
